d15caab2cab5400c3f23b9c7126952a0555075a3
TeggeAtze <niclaasmoldenhauer@googlemail.com>
2024-07-26 11:35:35 +0200

    added: globalContext

---
+++ client/context/globalContext.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const globalContext = React.createContext ();
+
+export const GlobalProvider = ({children}) => {
+  const [currentView, setCurrentView] = React.useState ('allChats');
+  const [showFriendProfile, setShowFriendProfile] = React.useState (false);
+  const [showProfile, setShowProfile] = React.useState (false);
+
+  const handleProfileToggle = show => {
+    setShowProfile (show);
+  };
+
+  const handleViewChange = view => {
+    setCurrentView (view);
+  };
+
+  const handleFriendProfile = show => {
+    setShowFriendProfile (show);
+  };
+
+  return (
+    <globalContext.Provider
+      value={{
+        currentView,
+        handleViewChange,
+        showProfile,
+        handleProfileToggle,
+        showFriendProfile,
+        handleFriendProfile,
+      }}
+    >
+      {children}
+    </globalContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return React.useContext (globalContext);
+};
