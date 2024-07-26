41ba186265e6efe6022a2aa2ad4667448520f104
TeggeAtze <niclaasmoldenhauer@googlemail.com>
2024-07-26 11:47:20 +0200

    updated: userProvider hook

--- client/providers/UserProvider.tsx
+++ client/providers/UserProvider.tsx
@@ -1,12 +1,18 @@
 "use client";
 import React from "react";
 import { UserContextProvider } from "../context/userContext";
+import { GlobalProvider } from "../context/globalContext";
 
 interface Props {
   children: React.ReactNode;
 }
 
 function UserProvider({ children }: Props) {
-  return <UserContextProvider>{children}</UserContextProvider>;}
+  return (
+    <UserContextProvider>
+      <GlobalProvider>{children}</GlobalProvider>
+    </UserContextProvider>
+  );
+}
 
 export default UserProvider;
